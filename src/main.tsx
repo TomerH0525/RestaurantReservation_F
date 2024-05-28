14bfec4f5b241f26d23f9e27575434819ae7a3d8
Tomer2H <tomer2b0525@gmail.com>
2024-05-28 21:08:01 +0300

    added main component to and style it instead of index

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,27 @@ import App from "./App.tsx";
 import "./index.css";
 import { Routing } from "./components/Layout/Routing/Routing.tsx";
 import { BrowserRouter } from "react-router-dom";
+import { Footer } from "./components/Layout/Footer/Footer.tsx";
+import { Header } from "./components/Layout/Header/Header.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <Routing />
+      <div className="h-screen w-screen flex flex-col overflow-auto">
+
+        <header className="sticky top-0">
+          <Header />
+        </header>
+
+        <body className="flex-1 p-5">
+          <Routing />
+        </body>
+
+        <footer className="sticky bottom-0 text-center">
+          <Footer />
+        </footer>
+
+      </div>
     </BrowserRouter>
   </React.StrictMode>
 );
